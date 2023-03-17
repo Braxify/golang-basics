fdd62919e2b6478122d2a4e98474e74ca231d0c2
Arthur <art.nazarian1@gmail.com>
2023-03-17 19:33:20 +0200

    check the timeouts

--- go-by-example/timeouts.go
+++ go-by-example/timeouts.go
@@ -9,7 +9,7 @@ func main() {
 
 	c1 := make(chan string, 1)
 	go func() {
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 		c1 <- "result 1"
 	}()
 
@@ -22,7 +22,7 @@ func main() {
 
 	c2 := make(chan string, 1)
 	go func() {
-		time.Sleep(2 * time.Second)
+		// time.Sleep(2 * time.Second)
 		c2 <- "result 2"
 	}()
 	select {
