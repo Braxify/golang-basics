19dfbe193b19de68fd79364a180870534fb849b4
Arthur <art.nazarian1@gmail.com>
2023-04-04 10:07:37 +0300

    add new example

---
+++ common-elements.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	arr1 := []int{1, 2, 3, 4, 5}
+	arr2 := []int{5, 2}
+
+	commonElements := make(map[int]bool)
+
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+
+	wg.Add(len(arr1))
+
+	for _, val1 := range arr1 {
+		go func(val1 int) {
+			for _, val2 := range arr2 {
+				if val1 == val2 {
+					mutex.Lock()
+					commonElements[val1] = true
+					mutex.Unlock()
+					break
+				}
+			}
+			wg.Done()
+		}(val1)
+	}
+
+	// Очікуємо завершення роботи всіх горутин
+	wg.Wait()
+
+	fmt.Println("Кількість спільних елементів:", len(commonElements))
+
+	if len(commonElements) > 0 {
+		fmt.Print("Спільні елементи: ")
+		for val := range commonElements {
+			fmt.Print(val, " ")
+		}
+	}
+
+	fmt.Println()
+}
